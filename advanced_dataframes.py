b50b67c6db36b70aaaa1747d65f6324a9e47e88d
KyleTaylorGreen <kyle.taylor.green@gmail.com>
2022-04-21 11:53:12 -0500

    finished advanced_dataframes exercises

--- advanced_dataframes.py
+++ advanced_dataframes.py
@@ -2,228 +2,288 @@ from env import get_db_url
 import numpy as np
 from pydataset import data
 import pandas as pd
-
+branch = 'PT3'
 ############################ Exercise PT 1 ###############################
+if branch == 'PT1':
 
-# 4 Use your function to obtain a connection to the employees database.
+    # 4 Use your function to obtain a connection to the employees database.
 
-url = get_db_url('employees')
-sql = '''
-SELECT * 
-FROM employees
-LIMIT 15
-'''
-our_employees = pd.read_sql(sql, url)
-print(our_employees.head())
+    url = get_db_url('employees')
+    sql = '''
+    SELECT * 
+    FROM employees
+    LIMIT 15
+    '''
+    our_employees = pd.read_sql(sql, url)
+    print(our_employees.head())
 
-# 5a) Intentionally make a typo in the database url. What kind of error
-#     message do you see?
+    # 5a) Intentionally make a typo in the database url. What kind of error
+    #     message do you see?
 
-"""
-sqlalchemy.exc.OperationalError: (pymysql.err.OperationalError) (1044, 
-"Access denied for user 'jemison_1739'@'%' to database 'employes'")
-"""
-
-
-# 5b) Intentionally make an error in your SQL query. What does the error 
-#     message look like?
-
-"""
-sqlalchemy.exc.ProgrammingError: (pymysql.err.ProgrammingError) 
-(1146, "Table 'employees.employe' doesn't exist")
-"""
-
-# 6) Read the employees and titles tables into two separate DataFrames.
-
-titles_sql = """
-SELECT * 
-FROM titles
-"""
-
-titles = pd.read_sql(titles_sql, url)
-
-employees_sql = """
-SELECT *
-FROM employees
-"""
-
-employees_table = pd.read_sql(employees_sql, url)
-print(employees_table.head())
-print(titles.head())
+    """
+    sqlalchemy.exc.OperationalError: (pymysql.err.OperationalError) (1044, 
+    "Access denied for user 'jemison_1739'@'%' to database 'employes'")
+    """
 
 
-# 7) How many rows and columns do you have in each DataFrame? 
-#    Is that what you expected?
+    # 5b) Intentionally make an error in your SQL query. What does the error 
+    #     message look like?
+
+    """
+    sqlalchemy.exc.ProgrammingError: (pymysql.err.ProgrammingError) 
+    (1146, "Table 'employees.employe' doesn't exist")
+    """
+
+    # 6) Read the employees and titles tables into two separate DataFrames.
+
+    titles_sql = """
+    SELECT * 
+    FROM titles
+    """
+
+    titles = pd.read_sql(titles_sql, url)
+
+    employees_sql = """
+    SELECT *
+    FROM employees
+    """
+
+    employees_table = pd.read_sql(employees_sql, url)
+    print(employees_table.head())
+    print(titles.head())
 
 
-print(f"titles rows x col: {titles.shape}")
-print(f"employees rows x col: {employees_table.shape}")
+    # 7) How many rows and columns do you have in each DataFrame? 
+    #    Is that what you expected?
 
-"""
-titles rows x col: 443308 x 4
-employees rows x col: 300024 x 6
-It's what I expected.
-"""
 
-# 8) Display the summary statistics for each DataFrame.
+    print(f"titles rows x col: {titles.shape}")
+    print(f"employees rows x col: {employees_table.shape}")
 
-print(titles.describe())
-print(employees_table.describe())
+    """
+    titles rows x col: 443308 x 4
+    employees rows x col: 300024 x 6
+    It's what I expected.
+    """
 
-# 9) How many unique titles are in the titles DataFrame?
+    # 8) Display the summary statistics for each DataFrame.
 
-print(f"Unique titles: {len(titles.title.unique())}")
+    print(titles.describe())
+    print(employees_table.describe())
 
-""" Answer: 7 """
+    # 9) How many unique titles are in the titles DataFrame?
 
-# 10) What is the oldest date in the to_date column?
+    print(f"Unique titles: {len(titles.title.unique())}")
 
-print(f"oldest date in to-date col: {titles.to_date.min()}")
+    """ Answer: 7 """
 
-""" Answer: 1985-03-01 """
+    # 10) What is the oldest date in the to_date column?
 
-# 11) What is the most recent date in the to_date column?
+    print(f"oldest date in to-date col: {titles.to_date.min()}")
 
-recent_dates = ~titles.to_date.astype('str').str.contains('9999')
-print(f"Most recent date in to-date col: {titles[recent_dates].to_date.max()}")
+    """ Answer: 1985-03-01 """
 
-""" Answer: 2002-08-01 """
+    # 11) What is the most recent date in the to_date column?
 
-other_title = """
-SELECT MAX(to_date) FROM (SELECT to_date FROM titles WHERE YEAR(to_date) <> '9999') as NewDate;
-"""
-print(f"most recent date: {pd.read_sql(other_title, url)}")
+    recent_dates = ~titles.to_date.astype('str').str.contains('9999')
+    print(f"Most recent date in to-date col: {titles[recent_dates].to_date.max()}")
+
+    """ Answer: 2002-08-01 """
+
+    other_title = """
+    SELECT MAX(to_date) FROM (SELECT to_date FROM titles WHERE YEAR(to_date) <> '9999') as NewDate;
+    """
+    print(f"most recent date: {pd.read_sql(other_title, url)}")
+
+
+
 
 ############################ Exercise PT 2 ###############################
 
+if branch == 'PT2':
+    # 1) Copy the users and roles DataFrames from the examples above.
+    # Create the roles/users DataFrames
 
-# 1) Copy the users and roles DataFrames from the examples above.
-# Create the roles/users DataFrames
+    roles = pd.DataFrame({
+        'id': [1, 2, 3, 4],
+        'name': ['admin', 'author', 'reviewer', 'commenter']
+    })
+    roles
 
-roles = pd.DataFrame({
-    'id': [1, 2, 3, 4],
-    'name': ['admin', 'author', 'reviewer', 'commenter']
-})
-roles
+    users = pd.DataFrame({
+        'id': [1, 2, 3, 4, 5, 6],
+        'name': ['bob', 'joe', 'sally', 'adam', 'jane', 'mike'],
+        'role_id': [1, 2, 3, 3, np.nan, np.nan]
+    })
+    users
 
-users = pd.DataFrame({
-    'id': [1, 2, 3, 4, 5, 6],
-    'name': ['bob', 'joe', 'sally', 'adam', 'jane', 'mike'],
-    'role_id': [1, 2, 3, 3, np.nan, np.nan]
-})
-users
+    # 2) What is the result of using a right join on the DataFrames?
 
-# 2) What is the result of using a right join on the DataFrames?
+    #users.merge(roles, left_on='role_id', right_on='id', how='right')
+    #print(users)
+    """
+    id   name  role_id
+    0   1    bob      1.0
+    1   2    joe      2.0
+    2   3  sally      3.0
+    3   4   adam      3.0
+    4   5   jane      NaN
+    5   6   mike      NaN
+    """
 
-#users.merge(roles, left_on='role_id', right_on='id', how='right')
-#print(users)
-"""
-   id   name  role_id
-0   1    bob      1.0
-1   2    joe      2.0
-2   3  sally      3.0
-3   4   adam      3.0
-4   5   jane      NaN
-5   6   mike      NaN
-"""
+    # 3) What is the result of using an outer join on the DataFrames?
 
-# 3) What is the result of using an outer join on the DataFrames?
+    #users.merge(roles, left_on='role_id', right_on='id', how='outer')
+    #print(users)
 
-#users.merge(roles, left_on='role_id', right_on='id', how='outer')
-#print(users)
+    """
+    id   name  role_id
+    0   1    bob      1.0
+    1   2    joe      2.0
+    2   3  sally      3.0
+    3   4   adam      3.0
+    4   5   jane      NaN
+    5   6   mike      NaN
+    """
 
-"""
-   id   name  role_id
-0   1    bob      1.0
-1   2    joe      2.0
-2   3  sally      3.0
-3   4   adam      3.0
-4   5   jane      NaN
-5   6   mike      NaN
-"""
+    # 4 What happens if you drop the foreign keys from the DataFrames and 
+    # try to merge them?
+    """
+    users.drop('role_id')
+    roles.drop('id')
 
-# 4 What happens if you drop the foreign keys from the DataFrames and 
-# try to merge them?
-"""
-users.drop('role_id')
-roles.drop('id')
-
-users.merge(roles, left_on='role_id', right_on='role_id')
-print(users)
-"""
-# KeyError: "['role_id'] not found in axis"
-
-
-
-# 5) Load the mpg dataset from PyDataset.
-
-mpg = data('mpg', show_doc=True)
-mpg = data('mpg')
-
-# 6) Output and read the documentation for the mpg dataset.
-
-# see above
-
-
-# 7) How many rows and columns are in the dataset?
-
-
-# 234 rows, 11 columns see above for code
+    users.merge(roles, left_on='role_id', right_on='role_id')
+    print(users)
+    """
+    # KeyError: "['role_id'] not found in axis"
 
 
 
-# 8) Check out your column names and perform any cleanup you may want on 
-#    them.
+    # 5) Load the mpg dataset from PyDataset.
 
-print(mpg.head())
+    mpg = data('mpg', show_doc=True)
+    mpg = data('mpg')
+
+    # 6) Output and read the documentation for the mpg dataset.
+
+    # see above
 
 
-# 9) Display the summary statistics for the dataset.
-mpg.describe()
+    # 7) How many rows and columns are in the dataset?
 
-# 10) How many different manufacturers are there?
 
-print(f"# of different manufacturers: {len(mpg.manufacturer.unique())}")
+    # 234 rows, 11 columns see above for code
 
-# 11) How many different models are there?
 
-print(f"# of different models: {len(mpg.model.unique())}")
 
-# 12) Create a column named mileage_difference like you did in the
-#     DataFrames exercises; this column should contain the difference 
-#     between highway and city mileage for each car.
+    # 8) Check out your column names and perform any cleanup you may want on 
+    #    them.
 
-mpg['mileage_difference'] = mpg.hwy - mpg.cty
+    print(mpg.head())
 
-# 13) Create a column named average_mileage like you did in the DataFrames 
-#     exercises; this is the mean of the city and highway mileage.
 
-mpg['average_mileage'] = (mpg.hwy + mpg.cty) / 2
+    # 9) Display the summary statistics for the dataset.
+    mpg.describe()
 
-# 14) Create a new column on the mpg dataset named is_automatic that holds
-#     boolean values denoting whether the car has an automatic transmission.
+    # 10) How many different manufacturers are there?
 
-mpg['is_automatic'] = mpg.trans.str.contains('auto')
+    print(f"# of different manufacturers: {len(mpg.manufacturer.unique())}")
 
-# 15) Using the mpg dataset, find out which which manufacturer has the best 
-#     miles per gallon on average?
+    # 11) How many different models are there?
 
-print(f"manufacturer with best mpg on average: {mpg.groupby(by='manufacturer').average_mileage.mean().idxmax()}")
+    print(f"# of different models: {len(mpg.model.unique())}")
 
-# 16) Do automatic or manual cars have better miles per gallon?
+    # 12) Create a column named mileage_difference like you did in the
+    #     DataFrames exercises; this column should contain the difference 
+    #     between highway and city mileage for each car.
 
-autos = mpg.trans.str.contains('auto')
-manuals = ~autos
+    mpg['mileage_difference'] = mpg.hwy - mpg.cty
 
-if mpg[autos].average_mileage.mean() > mpg[manuals].average_mileage.mean():
-    print('autos have better mpg')
-else:
-    print('manuals have better mpg')
+    # 13) Create a column named average_mileage like you did in the DataFrames 
+    #     exercises; this is the mean of the city and highway mileage.
 
-# alternatively
+    mpg['average_mileage'] = (mpg.hwy + mpg.cty) / 2
 
-# do automatics have higher mpg than manuals
-print(mpg.groupby(by='is_automatic').average_mileage.mean().idxmax())
-# False
+    # 14) Create a new column on the mpg dataset named is_automatic that holds
+    #     boolean values denoting whether the car has an automatic transmission.
 
-print(mpg.head())
+    mpg['is_automatic'] = mpg.trans.str.contains('auto')
+
+    # 15) Using the mpg dataset, find out which which manufacturer has the best 
+    #     miles per gallon on average?
+
+    print(f"manufacturer with best mpg on average: {mpg.groupby(by='manufacturer').average_mileage.mean().idxmax()}")
+
+    # 16) Do automatic or manual cars have better miles per gallon?
+
+    autos = mpg.trans.str.contains('auto')
+    manuals = ~autos
+
+    if mpg[autos].average_mileage.mean() > mpg[manuals].average_mileage.mean():
+        print('autos have better mpg')
+    else:
+        print('manuals have better mpg')
+
+    # alternatively
+
+    # do automatics have higher mpg than manuals
+    print(mpg.groupby(by='is_automatic').average_mileage.mean().idxmax())
+    # False
+
+    print(mpg.head())
+
+
+
+############################ Exercise PT 1 ###############################
+
+if branch == 'PT3':
+
+    # 1) Use your get_db_url function to help you explore the data from the 
+    #    chipotle database.
+    url = get_db_url('chipotle')
+    chipotle = pd.read_sql('SELECT * FROM orders', url)
+    print(chipotle.describe())
+    print(chipotle.info())
+
+    # 2) What is the total price for each order?
+
+    chipotle.item_price = chipotle.item_price.str.replace('$', '').astype('float')
+
+    print(chipotle.groupby('order_id').item_price.sum())
+
+    # with a pivot_table
+    print(chipotle.pivot_table(index='order_id', values='item_price', aggfunc='sum'))
+
+
+    # 3) What are the most popular 3 items?
+    print(chipotle.groupby('item_name').quantity.sum().nlargest(n=3, keep='all'))
+
+
+    # 4) Which item has produced the most revenue?
+
+    print(chipotle.groupby('item_name').item_price.sum().idxmax())
+
+    # 5) Join the employees and titles DataFrames together.
+
+    url = get_db_url('employees')
+    sql = """
+    SELECT dept_name, title, hire_date
+    FROM employees
+    JOIN titles USING(emp_no)
+    JOIN dept_emp USING(emp_no)
+    JOIN departments USING(dept_no);
+    """
+    emp_titles_joined = pd.read_sql(sql, url)
+    print(emp_titles_joined.head())
+
+    # 6) For each title, find the hire date of the employee that was hired 
+    #    most recently with that title.
+
+    print(emp_titles_joined.groupby('title').hire_date.max())
+
+    # 7) Write the code necessary to create a cross tabulation of the number 
+    # of titles by department. (Hint: this will involve a combination of SQL 
+    # code to pull the necessary data and python/pandas code to perform the manipulations.)
+
+    num_titles_by_dept = pd.crosstab(index=emp_titles_joined.dept_name, columns=emp_titles_joined.title)
+    print(num_titles_by_dept)
